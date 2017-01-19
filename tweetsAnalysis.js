9612fa0923e7c1a258f2394f8a7ea5355b2533ca
adenmosun <adenmosun@users.noreply.github.com>
2017-01-19 16:46:23 +0000

    Added process.en

--- tweetsAnalysis.js
+++ tweetsAnalysis.js
@@ -1,23 +1,24 @@
 'use strict'
 
-var watson = require('watson-developer-cloud');
-var alchemy_language = watson.alchemy_language({
-  api_key: '753422bdb920d98776a725a24ddbdd9c14ebbb3f'
-})
-
+require('dotenv').config();
 var Twitter = require('twitter');
 var rl = require('readline');
 var sw = require('stopword');
-var Table = require('easy-table');
 var consoleTable = require('console.table');
+
+var watson = require('watson-developer-cloud');//alchemyapi for sentiment analysis
+var alchemy_language = watson.alchemy_language({
+  api_key: process.env.API_KEY
+})
  
 var client = new Twitter({
-  consumer_key: 'kNWbOtCaNjp7rXajQcs0A0UJB',
-  consumer_secret: '9xnaeb0oHXJlwBvMTHXPc7v1ftjgrpRqkpUD6aMkTHO96tG1fU',
-  access_token_key: '821062173959028736-kMnEV9zjAyY8IMMP3iRy2JTRHHSYe9j',
-  access_token_secret: '8tMzx9qApEhOUiSP9W7KpGVOSFYmHfKHCCBBio78vIGx2',
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
- 
+
+
  var read = rl.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -35,12 +36,11 @@ var client = new Twitter({
             console.log('Tweets loading' + ' ' + Math.round(progress/data.length * 100) + '%');
 }
 
-
   const oldString = tweets.toLowerCase().replace(/[^a-z\s]+/g, '').split(' ');
   const word = sw.removeStopwords(oldString); //Remove stop-words from tweets
     var wordCount = {};
       for (var i = 0; i < word.length; i++){
-        wordCount[word[i]] = (wordCount[word[i]]) ? wordCount[word[i]] + 1 : 1; //To create a table of words in tweets and frequency
+        wordCount[word[i]] = (wordCount[word[i]]) ? wordCount[word[i]] + 1 : 1; //To create a table of words in the tweets and their frequency
 }
 
 
@@ -48,7 +48,7 @@ var client = new Twitter({
   var words = [];
     for (var i in wordCount)
       words.push([i, wordCount[i]])
-      words.sort(function(a, b) { return b[1] - a[1]; //To sort words based on frequency 
+      words.sort(function(a, b) { return b[1] - a[1]; //To sort words based on frequency from highest to lowest
 })
       console.log("hey!!!!!")
       console.table('words    frequency', words.slice(0, 21));
@@ -66,9 +66,8 @@ var client = new Twitter({
         for (var key in docSentiment){
           sentiment.push([key, docSentiment[key]])
  }
+}
         console.table('sentiment    value', sentiment); //sentiment analysis with alchemy
-        //console.table(JSON.stringify(response, null, 2));
- }
          
 });
 });
