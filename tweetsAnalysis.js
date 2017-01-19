2800c8f020fcfc1de68b3d0a4fbb0de67b280538
adenmosun <adenmosun@users.noreply.github.com>
2017-01-19 13:12:47 +0000

    print sentiment analysis as a table

--- tweetsAnalysis.js
+++ tweetsAnalysis.js
@@ -27,25 +27,22 @@ var client = new Twitter({
   read.close();
   var options = {screen_name: answer, count: 500};
     client.get('statuses/user_timeline', options, function (err, data, response) {
-      //console.log(data);
     var tweets = '';
       console.log('Tweets' + ' ' + data.length);
         for (var i = 0; i < data.length; i++){
           var progress = i++;
             tweets += data[i].text;
             console.log('Tweets loading' + ' ' + Math.round(progress/data.length * 100) + '%');
-            //console.log(tweets);
 }
+
 
   const oldString = tweets.toLowerCase().replace(/[^a-z\s]+/g, '').split(' ');
   const word = sw.removeStopwords(oldString); //Remove stop-words from tweets
-  //console.log(word);
     var wordCount = {};
       for (var i = 0; i < word.length; i++){
         wordCount[word[i]] = (wordCount[word[i]]) ? wordCount[word[i]] + 1 : 1; //To create a table of words in tweets and frequency
 }
-        // console.log(wordCount);
-      // return wordCount;
+
 
 
   var words = [];
@@ -55,14 +52,24 @@ var client = new Twitter({
 })
       console.log("hey!!!!!")
       console.table('words    frequency', words.slice(0, 21));
-      
+ 
+
   var parameters = {
     url: 'www.twitter.com/' + answer + '/'  };
     alchemy_language.sentiment(parameters, function (err, response) {
-      if (err)
+      if (err) {
         console.log('error:', err);
-      else
-        console.log(JSON.stringify(response, null, 2)); //sentiment analysis with alchemy
+      }
+      else {
+        var docSentiment = response.docSentiment
+        var sentiment = []
+        for (var key in docSentiment){
+          sentiment.push([key, docSentiment[key]])
+ }
+        console.table('sentiment    value', sentiment); //sentiment analysis with alchemy
+        //console.table(JSON.stringify(response, null, 2));
+ }
+         
 });
 });
 });
